3566234af9773ca1bc96ab19771b27440755ee03
natgsarabia <natalia.garcia@estudiant.fjaverianas.com>
2024-05-12 14:47:19 +0200

    trabajo word

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for, session, flash, send_from_directory
+from flask import Flask,render_template,request,redirect,url_for, session, flash
 import mysql.connector as mysql
 import random
 import matplotlib
